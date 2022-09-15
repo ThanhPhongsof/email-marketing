15d06ebd9ceede56d18fca6edf66aeaf97fc3b43
ThanhPhongsof <ttphongletter@gmail.com>
2022-09-15 20:52:29 +0700

    feat/main : add section service in desktop

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -1,1 +1,4 @@
 export const { default: IconPlay } = require("./IconPlay");
+export const { default: IconPopups } = require("./IconPopups");
+export const { default: IconSMS } = require("./IconSMS");
+export const { default: IconMail } = require("./IconMail");
