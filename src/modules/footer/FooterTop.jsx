0962aa3b23b7fbeae3a81fcd9910fea9277de19a
ThanhPhongsof <ttphongletter@gmail.com>
2022-09-16 09:49:02 +0700

    feat/footer : add footer in desktop

---
+++ src/modules/footer/FooterTop.jsx
@@ -0,0 +1,15 @@
+const FooterTop = () => {
+  return (
+    <div className="footer-top">
+      <h2 className="heading heading--big">See it for yourself</h2>
+      <p className="text">
+        Cat in touch with of our compare and take a personal tour of Sraklune
+      </p>
+      <a href="#!" alt="" className="button button--primary">
+        Request a Demo
+      </a>
+    </div>
+  );
+};
+
+export default FooterTop;
