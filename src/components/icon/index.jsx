1445da6978ab3bde4be2bc2ba23fd6467945b2d4
ThanhPhongsof <ttphongletter@gmail.com>
2022-09-16 08:41:47 +0700

    feat/main : add section business-tools in desktop

--- src/components/icon/index.jsx
+++ src/components/icon/index.jsx
@@ -2,3 +2,4 @@ export const { default: IconPlay } = require("./IconPlay");
 export const { default: IconPopups } = require("./IconPopups");
 export const { default: IconSMS } = require("./IconSMS");
 export const { default: IconMail } = require("./IconMail");
+export const { default: IconRight } = require("./IconRight");
